968e9a2a9f9a142de9e7c47b1fc76192082d69c3
Aaron William Langham <awlangham@gmail.com>
2020-07-19 18:16:37 -0400

    Update Express.java

--- src/Express.java
+++ src/Express.java
@@ -1,26 +1,10 @@
-public class Express {
-	public static void main(String[] args) {
-		int seasonsOver400 = 0;
-		for (int i = 0; i < numSamples; ++i) {
-			if (playSeason() >= 0.400) {
-				++seasonsOver400;
-			}
-		}
-		double oddsOfOver400 = ((double) seasonsOver400) / numSamples;
-		System.out.println("For N = " + numSamples + ": " + oddsOfOver400);
-	}
-
+class Season {
 	private static final double battingAverage = 0.350;
 	private static final int numAtBats = 4;
 	private static final int numGames = 60;
-	private static final int numSamples = 1000000;
 
 	private static boolean swing() {
-		if (Math.random() < battingAverage) {
-			return true;
-		} else {
-			return false;
-		}
+		return Math.random() < battingAverage;
 	}
 
 	private static int playGame() {
@@ -33,11 +17,26 @@ public class Express {
 		return hits;
 	}
 
-	private static double playSeason() {
+	public static double playSeason() {
 		int hits = 0;
 		for (int i = 0; i < numGames; ++i) {
 			hits += playGame();
 		}
 		return ((double) hits) / (numAtBats * numGames);
+	}
+}
+
+public class Express {
+	public static void main(String[] args) {
+		Season s1 = new Season();
+		int numSamples = 1000000;
+		int seasonsOver400 = 0;
+		for (int i = 0; i < numSamples; ++i) {
+			if (s1.playSeason() >= 0.400) {
+				++seasonsOver400;
+			}
+		}
+		double oddsOfOver400 = ((double) seasonsOver400) / numSamples;
+		System.out.println("For N = " + numSamples + ": " + oddsOfOver400);
 	}
 }
